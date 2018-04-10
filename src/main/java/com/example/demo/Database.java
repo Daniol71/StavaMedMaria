d5ce41da5b23a15d9dfc9457686230c45fed2225
Daniel Lecoq <daninstockings@gmail.com>
2018-04-10 14:51:59 +0200

    Create Database class and add connection + sql statement placeholder

---
+++ src/main/java/com/example/demo/Database.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+    @Autowired
+    Connection dbconn;
+
+    public static void viewTable(Connection con, String dbName)
+            throws SQLException {
+    Statement stmt = null;
+    String query = "select COF_NAME, SUP_ID, PRICE, " +
+            "SALES, TOTAL " +
+            "from " + dbName + ".COFFEES";
+    try {
+        stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        while (rs.next()) {
+            String coffeeName = rs.getString("COF_NAME");
+            int supplierID = rs.getInt("SUP_ID");
+            float price = rs.getFloat("PRICE");
+            int sales = rs.getInt("SALES");
+            int total = rs.getInt("TOTAL");
+            System.out.println(coffeeName + "\t" + supplierID +
+                    "\t" + price + "\t" + sales +
+                    "\t" + total);
+        }
+    } catch (SQLException e ) {
+        e.printStackTrace()
+    } finally {
+        if (stmt != null) { stmt.close(); }
+    }
+}
+}
