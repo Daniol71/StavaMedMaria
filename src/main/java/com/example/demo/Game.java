8ec7ddd5b91376469adb866f2659503f661b410f
Daniel Lecoq <daninstockings@gmail.com>
2018-04-11 10:31:59 +0200

    Create Game.class

---
+++ src/main/java/com/example/demo/Game.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+public class Game {
+
+    /*public boolean inputValidator(String input) {
+        input = input.trim();
+        if (input == null || input.isEmpty()) {
+            return false;
+        }
+        if (input.matches("0123456789")){
+
+        }
+        return true;
+    }*/
+
+    /*public boolean stringComparator(String input, String correct){
+        input = input.trim();
+
+        int correctLength = correct.length();
+        int inputLength = input.length();
+
+        if(inputLength==correctLength){
+
+        }
+
+
+        return true;
+    }*/
+}
