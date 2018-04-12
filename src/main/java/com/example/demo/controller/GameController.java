5a666dd5dfc3ec579ba59f4e836a05762c7247f6
Alexander Kruse <ak222zn@student.lnu.se>
2018-04-12 13:59:31 +0200

    Add action, methid and name to form. Create @PostMapping and connect to method stringComparator

--- src/main/java/com/example/demo/controller/GameController.java
+++ src/main/java/com/example/demo/controller/GameController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.sql.DataSource;
@@ -24,16 +26,20 @@ public class GameController {
 
     @GetMapping("/Difficulty.html")
     public ModelAndView showDifficulty(){
-        try {
+      /*  try {
             run.test();
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        }*/
         return new ModelAndView("Difficulty.html");
     }
 
     @GetMapping("/MainGamePage.html")
     public ModelAndView showMainGamePage(){
         return new ModelAndView("MainGamePage.html");
+    }
+    @PostMapping("/WordInput")
+    public ModelAndView inputWord(@RequestParam String input, String correct ){ game.stringComparator(input, correct);
+        return new ModelAndView("WordInput");
     }
 }
